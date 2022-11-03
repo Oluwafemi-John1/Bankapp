f99ca7a2e41391e7fbe757c91b6b81ca02d4e649
Oluwafemi John Oyeniran <oyeniranoluwafemi36@gmail.com>
2022-11-03 16:06:18 +0100

    Files created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/Homepage';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Homepage/>}/>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/signin' element={<Signin/>}/>
+        </Routes>
+    </Router>
+    </>
   );
 }
 
