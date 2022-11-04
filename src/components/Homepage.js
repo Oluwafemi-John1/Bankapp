4898bfdc744c05e11c992cd9073bc70cb3f12505
Oluwafemi John Oyeniran <oyeniranoluwafemi36@gmail.com>
2022-11-04 12:32:44 +0100

    Navbar completed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Homepage = () => {
   return (
     <>
+        <Navbar/>
         <div>Homepage</div>
     </>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/images/svgexport-1.svg';
+import logo from '../assets/images/nova logo.png';
 import kudacard from '../assets/images/svgexport-3.svg';
 import spend from '../assets/images/svgexport-4.svg';
 import save from '../assets/images/svgexport-5.svg';
@@ -8,14 +8,15 @@ import borrow from '../assets/images/svgexport-7.svg';
 import countrylogo from '../assets/images/logo.svg';
 import imagescam from '../assets/images/imagescam.png';
 import { Link } from 'react-router-dom';
+import '../assets/css/Navbar.css';
 
 const Navbar = () => {
   return (
     <>
         <div className="sticky-top">
-            <nav className="navbar navbar-expand-lg py-3">
+            <nav className="navbar navbar-expand-lg py-3 shadow-sm">
                 <div className="container-fluid">
-                    <a className="navbar-brand ms-lg-5" href="#"><img src={logo} width={85} alt="" className='imglogo'/></a>
+                    <a className="navbar-brand ms-lg-5" href="#"><img src={logo} width={90} alt="" className='imglogo'/></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -26,37 +27,37 @@ const Navbar = () => {
                             <li className="nav-item dropdown px-1">
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >Features</a>
                                 <ul className="dropdown-menu border-0 shadow-sm">
-                                    <li className='py-1'><a href="" className="dropdown-item"><img src={kudacard} width={25} alt="" />&nbsp;&nbsp;&nbsp;Kuda Card</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item"><img src={spend} width={25} alt="" />&nbsp;&nbsp;&nbsp;Spend</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item"><img src={save} width={25} alt="" />&nbsp;&nbsp;&nbsp;Save</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item"><img src={budget} width={25} alt="" />&nbsp;&nbsp;&nbsp;Budget</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item"><img src={borrow} width={25} alt="" />&nbsp;&nbsp;&nbsp;Borrow</a></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item"><img src={kudacard} width={25} alt="" />&nbsp;&nbsp;&nbsp;Nova Card</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item"><img src={spend} width={25} alt="" />&nbsp;&nbsp;&nbsp;Spend</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item"><img src={save} width={25} alt="" />&nbsp;&nbsp;&nbsp;Save</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item"><img src={budget} width={25} alt="" />&nbsp;&nbsp;&nbsp;Budget</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item"><img src={borrow} width={25} alt="" />&nbsp;&nbsp;&nbsp;Borrow</Link></li>
                                 </ul>
                             </li>
 
                             <li className="nav-item">
-                            <a className="nav-link" href="#">Business&nbsp;<a href="" className='border rounded text-decoration-none shadow-sm'><span className='Beta'>Beta</span></a></a>
+                            <Link className="nav-link" href="#">Business&nbsp;<a href='#' className='border rounded text-decoration-none shadow-sm'><span className='Beta'>Beta</span></a></Link>
                             </li>
                             
                             {/* Company */}
                             <li className="nav-item dropdown px-1">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >Company</a>
+                                <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >Company</Link>
                                 <ul className="dropdown-menu border-0 shadow-sm">
-                                    <li className='py-1'><a href="" className="dropdown-item">Blog</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">Press</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">Join our team</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">About us</a></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Blog</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Press</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Join our team</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">About us</Link></li>
                                 </ul>
                             </li>
 
                             {/* Get Help */}
                             <li className="nav-item dropdown px-1">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >Help</a>
+                                <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >Help</Link>
                                 <ul className="dropdown-menu border-0 shadow-sm">
-                                    <li className='py-1'><a href="" className="dropdown-item">Get Help</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">FAQs</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">Security</a></li>
-                                    <li className='py-1'><a href="" className="dropdown-item">Contact us</a></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Get Help</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">FAQs</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Security</Link></li>
+                                    <li className='py-1'><Link href="" className="dropdown-item">Contact us</Link></li>
                                 </ul>
                             </li>
                         </ul>
@@ -66,12 +67,12 @@ const Navbar = () => {
                                 <Link className="nav-link" to="/signin">Sign In</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link text-light rounded px-4" to="/signup" id='getKuda'>Get Kuda</Link>
+                                <Link className="nav-link text-light rounded px-4" to="/signup" id='getKuda'>Get Nova</Link>
                             </li>
                             <li className="nav-item dropdown" id='imagescammer'>
-                                <a className="nav-link " href="#"><img src={countrylogo} alt="" /></a>
+                                <Link className="nav-link " to=""><img src={countrylogo} alt="" /></Link>
                                 <ul className='dropdown-menu border-0 rounded'>
-                                    <li id='imagescam'><a href="" className="dropdown-item"><img src={imagescam} alt="" /></a></li>
+                                    <li id='imagescam'><Link to="" className="dropdown-item"><img src={imagescam} alt="" /></Link></li>
                                 </ul>
                             </li>
                         </ul>
