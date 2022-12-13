de5e25b6e118bbcec7744c755ba1bd9241cc1ebe
Oluwafemi John Oyeniran <oyeniranoluwafemi36@gmail.com>
2022-12-13 22:37:41 +0100

    cards updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,7 +13,7 @@ const Cards = () => {
 
     <section className="container-fluid border-danger border-3 mt-5 mt-lg-0 mt-md-0" id='sectionCard'>
         {/* First section */}
-        <div className="row my-5 border border-warning" id='divCard'>
+        <div className="row my-5 border-bottom" id='divCard'>
             <div className="col-lg-4 col-sm-12 border-info mt-lg-5" id='freebank1'>
                 <h1 id='moneyapp' className='p-lg-2'>The money app for everyone</h1>
                 <p className='p-3' id='saveSpend'>Save, spend, send and invest money across borders.</p>
@@ -24,7 +24,7 @@ const Cards = () => {
             </div>
         </div>
         {/* Second Section */}
-        <div className="row my-5 border border-warning">
+        <div className="row my-5 border-bottom">
             <div className="col-lg-4 col-sm-12 border-info mt-lg-5" id='freebank1'>
                 <h1 id='moneyapp' className='p-lg-2'>Save</h1>
                 <p className='p-3' id='saveSpend'>Putting money away for the future is smart but it takes discipline - something humans don’t have in abundance. We’re using technology to solve that by automating saving. You set an amount to save and the Novaa app takes care of the rest, prompt interest payout included.</p>
@@ -34,7 +34,7 @@ const Cards = () => {
             </div>
         </div>
         {/* Third Section */}
-        <div className="row my-5 border border-warning">
+        <div className="row my-5 border-bottom">
             <div className="col-lg-5 col-sm-12 border-primary p-5" id='imgAtm'>
                 <img src={spend} alt="" width={500} className='img-fluid img-responsive' />
             </div>
